7995ddc8bdd65ab96544e6299b66de304fdabd03
yiyongfei <each51@hotmail.com>
2017-04-28 13:03:24 +0800

    Apache协议

--- yea-cache/src/main/java/com/yea/cache/jedis/pool/RedisGeneralPool.java
+++ yea-cache/src/main/java/com/yea/cache/jedis/pool/RedisGeneralPool.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2014 伊永飞
+ * Copyright 2017 伊永飞
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
